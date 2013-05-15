cf097355786ec9822e7b2a40a9cf77cc1aa39d78
Joao Barreiro <bejeca@gmail.com>
2013-05-15 15:23:02 +0200

    hello world

---
+++ src/main/java/biz/jbar/kumato/Kumato.java
@@ -0,0 +1,16 @@
+package biz.jbar.kumato;
+
+public class Kumato {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((new Kumato()).hello());
+	}
+
+	public String hello() {
+		return "Hello from Kumato!!!";
+	}
+}
