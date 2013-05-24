3bdadeca56d988e9f60eff36bd1001db4fcef529
Joao Barreiro <bejeca@gmail.com>
2013-05-25 00:49:40 +0200

    move cucumber tests to maven integration-test phase

--- kumato-atdd/src/test/java/net/kumato/atdd/CucumberIT.java
+++ kumato-atdd/src/test/java/net/kumato/atdd/CucumberIT.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 // @Cucumber.Options(features={"src/test/java/"})
+// @Cucumber.Options(format = { "json", "json:target/cucumber.json" }, features = { "." }, strict = true)
 @Cucumber.Options(format = { "pretty", "html:target/cucumber" })
-public class KumatoAcceptanceTest {
+public class CucumberIT {
 }
